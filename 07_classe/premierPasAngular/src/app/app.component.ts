c5a48c8439a924878af3dafbdebab0635dd5953e
Thiraiyan Mooneesawmy <Niseiyen@gmail.com>
2023-09-21 21:18:35 -0400

    premierPasAngular 07 08 09

--- 07_classe/premierPasAngular/src/app/app.component.ts
+++ 07_classe/premierPasAngular/src/app/app.component.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
+  
   monTexte = 'Bonjour à tous !';
-  boutonActif = true;
+  boutonActif: boolean = true;
+  monAutreTexte = 'NgModule !';
+  isAvailable = true;
+  monNombre = 16;
+
+  listeLivres = [
+    {
+        titre: 'Angular 14'
+    },
+    {
+        titre: 'Java 21'
+    },
+    {
+        titre: 'Svelte 3'
+    },
+    {
+        titre: 'Flutter 3'
+    },
+    {
+        titre: 'Dart 2'
+    },
+    {
+        titre: 'React 18'
+    }
+];
+
+  onBoutonClique() {
+    this.boutonActif = !this.boutonActif;
+  }
+  cachertexte() {
+    this.isAvailable = !this.isAvailable;
+  }
+
+  inputOfText(event: Event) {
+    const inputOfText = (event.target as HTMLInputElement).value;
+    console.log(inputOfText);
+    this.monTexte = inputOfText;
+  }
+  
+  effacerTexte() {
+    this.monTexte = '';
+  }
+
+  monCarre(valeur: number) {
+    return Math.sqrt(valeur);
+  }
+
+
 }
 
 
--- 07_classe/premierPasAngular/src/app/app.module.ts
+++ 07_classe/premierPasAngular/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AlphaComponent } from './alpha/alpha.component';
+import { BetaComponent } from './beta/beta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlphaComponent,
+    BetaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
